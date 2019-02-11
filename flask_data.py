c77ded15021fb56433b26e36dca929d9080a4655
patam01 <47400661+patam01@users.noreply.github.com>
2019-02-11 16:48:18 -0500

    Add files via upload

---
+++ flask_data.py
@@ -0,0 +1,37 @@
+from ddtrace import tracer
+from ddtrace.contrib.requests import patch
+patch()
+
+import requests
+
+with tracer.trace('root.span', service='citidirectapp'):
+    requests.get("http://www.datadog.com")
+
+from flask import Flask
+import logging
+import sys
+
+# Have flask use stdout as the logger
+main_logger = logging.getLogger()
+main_logger.setLevel(logging.DEBUG)
+c = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c.setFormatter(formatter)
+main_logger.addHandler(c)
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_entry():
+    return 'Entrypoint to the Application'
+
+@app.route('/api/apm')
+def apm_endpoint():
+    return 'Getting APM Started'
+
+@app.route('/api/trace')
+def trace_endpoint():
+    return 'Posting Traces'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port='5050')
